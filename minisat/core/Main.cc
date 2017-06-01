16a08b0f95078ebdd8962895436be11ce4418236
Nenad <Ahhhhmed@gmail.com>
2017-06-01 21:24:50 +0200

    Add update answers in test.

--- minisat/core/Main.cc
+++ minisat/core/Main.cc
@@ -55,7 +55,7 @@ static void SIGINT_exit(int) {
 int main(int argc, char** argv)
 {
     try {
-        setUsageHelp("USAGE: %s [options] <input-file> <result-output-file>\n\n  where input may be either in plain or gzipped DIMACS.\n");
+        setUsageHelp("USAGE: %s [options] <input-file> <result-output- file>\n\n  where input may be either in plain or gzipped DIMACS.\n");
         setX86FPUPrecision();
 
         // Extra options:
--- minisat/test/simple_test.cpp
+++ minisat/test/simple_test.cpp
@@ -3,32 +3,56 @@
 #include <fstream>
 #include <zlib.h>
 #include "minisat/core/Dimacs.h"
+#include <string>
 
 using namespace Minisat;
+using std::string;
 
+const string samplesPath = "../minisat/test/samples/";
+const string answersExtension = ".answers.txt";
 
-TEST(fix, test1) {
+#define  UPDATE_ANSWERS
+
+TEST(solver, refactor) {
+    string fileName = "unif-r4.25-v400-c1700-05-S1956245623.used-as.sat04-35.cnf";
     Solver S;
-    gzFile in = gzopen("../minisat/test/tests/unif-r4.25-v400-c1700-05-S1956245623.used-as.sat04-35.cnf", "rb");
-    std::ifstream answers("../minisat/test/tests/asd.txt");
-    uint64_t expected_starts, expected_conflicts, expected_decisions, expected_propagations, expected_tot_literals;
-    answers >> expected_starts >> expected_conflicts >> expected_decisions >> expected_propagations >> expected_tot_literals;
+    gzFile in = gzopen((samplesPath + fileName).c_str(), "rb");
     if(in == NULL){
-        FAIL() << "Error reading file";
+        FAIL() << "Error reading cnf file";
     }
     parse_DIMACS(in, S);
+
+#ifdef UPDATE_ANSWERS
+    std::ofstream answers((samplesPath + fileName + answersExtension));
+    if(!answers.is_open()){
+        FAIL() << "Error creating answers file";
+    }
+#else
+    std::ifstream answers((samplesPath + fileName + answersExtension));
+    if(!answers.is_open()){
+        FAIL() << "Error reading answers file";
+    }
+    uint64_t expected_starts, expected_conflicts, expected_decisions, expected_propagations, expected_tot_literals;
+    answers >> expected_starts >> expected_conflicts >> expected_decisions >> expected_propagations >> expected_tot_literals;
+#endif
 
     if (S.simplify()){
         vec<Lit> dummy;
         lbool ret = S.solveLimited(dummy);
     }
 
+#ifdef UPDATE_ANSWERS
+    answers << S.starts << std::endl;
+    answers << S.conflicts << std::endl;
+    answers << S.decisions << std::endl;
+    answers << S.propagations << std::endl;
+    answers << S.tot_literals << std::endl;
+#else
     ASSERT_EQ(expected_starts, S.starts);
     ASSERT_EQ(expected_conflicts, S.conflicts);
     ASSERT_EQ(expected_decisions, S.decisions);
     ASSERT_EQ(expected_propagations, S.propagations);
     ASSERT_EQ(expected_tot_literals, S.tot_literals);
+#endif
 }
 
-TEST(fix, test2) {
-}
