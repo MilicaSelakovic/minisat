bf4a0a6fa4edbe4cd0bd13d4d3ccd6ff2f5643be
Nenad <Ahhhhmed@gmail.com>
2017-06-02 11:07:26 +0200

    Create test fixture.

--- minisat/test/simple_test.cpp
+++ minisat/test/simple_test.cpp
@@ -11,48 +11,54 @@ using std::string;
 const string samplesPath = "../minisat/test/samples/";
 const string answersExtension = ".answers.txt";
 
-#define  UPDATE_ANSWERS
+//#define  UPDATE_ANSWERS
 
-TEST(solver, refactor) {
-    string fileName = "unif-r4.25-v400-c1700-05-S1956245623.used-as.sat04-35.cnf";
-    Solver S;
-    gzFile in = gzopen((samplesPath + fileName).c_str(), "rb");
-    if(in == NULL){
-        FAIL() << "Error reading cnf file";
-    }
-    parse_DIMACS(in, S);
-
-#ifdef UPDATE_ANSWERS
-    std::ofstream answers((samplesPath + fileName + answersExtension));
-    if(!answers.is_open()){
-        FAIL() << "Error creating answers file";
-    }
-#else
-    std::ifstream answers((samplesPath + fileName + answersExtension));
-    if(!answers.is_open()){
-        FAIL() << "Error reading answers file";
-    }
-    uint64_t expected_starts, expected_conflicts, expected_decisions, expected_propagations, expected_tot_literals;
-    answers >> expected_starts >> expected_conflicts >> expected_decisions >> expected_propagations >> expected_tot_literals;
-#endif
-
-    if (S.simplify()){
-        vec<Lit> dummy;
-        lbool ret = S.solveLimited(dummy);
-    }
+class RefactorTest : public ::testing::Test{
+protected:
+    void testFromFile(string fileName){
+        Solver S;
+        gzFile in = gzopen((samplesPath + fileName).c_str(), "rb");
+        if(in == NULL){
+            FAIL() << "Error reading cnf file";
+        }
+        parse_DIMACS(in, S);
 
 #ifdef UPDATE_ANSWERS
-    answers << S.starts << std::endl;
-    answers << S.conflicts << std::endl;
-    answers << S.decisions << std::endl;
-    answers << S.propagations << std::endl;
-    answers << S.tot_literals << std::endl;
+        std::ofstream answers((samplesPath + fileName + answersExtension));
+        if(!answers.is_open()){
+            FAIL() << "Error creating answers file";
+        }
 #else
-    ASSERT_EQ(expected_starts, S.starts);
-    ASSERT_EQ(expected_conflicts, S.conflicts);
-    ASSERT_EQ(expected_decisions, S.decisions);
-    ASSERT_EQ(expected_propagations, S.propagations);
-    ASSERT_EQ(expected_tot_literals, S.tot_literals);
+            std::ifstream answers((samplesPath + fileName + answersExtension));
+        if(!answers.is_open()){
+            FAIL() << "Error reading answers file";
+        }
+        uint64_t expected_starts, expected_conflicts, expected_decisions, expected_propagations, expected_tot_literals;
+        answers >> expected_starts >> expected_conflicts >> expected_decisions >> expected_propagations >> expected_tot_literals;
 #endif
+
+        if (S.simplify()){
+            vec<Lit> dummy;
+            lbool ret = S.solveLimited(dummy);
+        }
+
+#ifdef UPDATE_ANSWERS
+        answers << S.starts << std::endl;
+        answers << S.conflicts << std::endl;
+        answers << S.decisions << std::endl;
+        answers << S.propagations << std::endl;
+        answers << S.tot_literals << std::endl;
+#else
+        ASSERT_EQ(expected_starts, S.starts);
+        ASSERT_EQ(expected_conflicts, S.conflicts);
+        ASSERT_EQ(expected_decisions, S.decisions);
+        ASSERT_EQ(expected_propagations, S.propagations);
+        ASSERT_EQ(expected_tot_literals, S.tot_literals);
+#endif
+    }
+};
+
+TEST_F(RefactorTest, test1) {
+    testFromFile("unif-r4.25-v400-c1700-05-S1956245623.used-as.sat04-35.cnf");
 }
 
